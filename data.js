230db4b511901c7da589380497388205ed09d3d6
ZezeBasmallah <168107474+ZezeBasmallah@users.noreply.github.com>
2024-06-27 10:01:37 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+const nama = "zeze basmallah";
+let usia = 23;
+
+function Biodata() {
+    if (usia >17  ) {
+        console.log(`anda sudah dewasa tpi ga boleh mabok`)
+    } else {
+        console.log(`anda masih bocah ingusan`)
+    }
+    console.log(`nama saya ${nama} usia saya ${usia} tahun`)
+}
+
+Biodata();
+
+console.log(nama);
+console.log(usia);
